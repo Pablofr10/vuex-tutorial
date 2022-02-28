c8f9918d0c2f77410ff596733864ab4bec896648
Pablorf10 <rafanet.live@gmail.com>
2022-02-28 13:40:59 -0300

    Finalizando projeto pinia

--- src/views/Favoritos.vue
+++ src/views/Favoritos.vue
@@ -16,54 +16,20 @@
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
+import {usuarioStore, loginStore} from '../store/cadastro'
+import { storeToRefs } from 'pinia';
 export default {
   components: {},
   setup() {
-    const listaFavoritos = ref([]);
+    const store = usuarioStore()
+    const login = loginStore()
+    const { listaFavoritos } = storeToRefs(store)
 
-    onMounted(() => {
-      listaFavoritos.value = [
-        {
-          id: 7,
-          email: "michael.lawson@reqres.in",
-          first_name: "Michael",
-          last_name: "Lawson",
-          avatar: "https://reqres.in/img/faces/7-image.jpg",
-        },
-        {
-          id: 8,
-          email: "lindsay.ferguson@reqres.in",
-          first_name: "Lindsay",
-          last_name: "Ferguson",
-          avatar: "https://reqres.in/img/faces/8-image.jpg",
-        },
-        {
-          id: 9,
-          email: "tobias.funke@reqres.in",
-          first_name: "Tobias",
-          last_name: "Funke",
-          avatar: "https://reqres.in/img/faces/9-image.jpg",
-        },
-        {
-          id: 10,
-          email: "byron.fields@reqres.in",
-          first_name: "Byron",
-          last_name: "Fields",
-          avatar: "https://reqres.in/img/faces/10-image.jpg",
-        },
-      ];
-    });
-
-    function removeFavorito(idUsuario) {
-      listaFavoritos.value = listaFavoritos.value.filter(
-        (x) => x.id !== idUsuario
-      );
-    }
+    console.log(login.nome)
 
     return {
       listaFavoritos,
-      removeFavorito,
+      removeFavorito: store.removeUsuario
     };
   },
 };
